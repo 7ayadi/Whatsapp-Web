2dd7684aed8273bf4466aeea087e6c685d3ed867
Luca <33968601+iSn0we@users.noreply.github.com>
2018-11-24 13:36:16 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,3 +1,17 @@
-fucntion sendMessage(text){
-  document.getElementsByClassName("_2S1VP").innerHTML = text;
+function sendMessage(text){
+  //insert the text
+  document.getElementsByClassName("_2S1VP")[0].innerHTML = text;
+    document.getElementsByClassName("_2S1VP")[0].innerTEXT = text;
+    // refresh to apply change
+    event = document.createEvent("UIEvents");
+		event.initUIEvent("input", true, true, window, 1);
+    messageBox.dispatchEvent(event);
+    // click the send button
+    $("._35EW6").click();
+}
+
+function lastMessage(){
+  var lastmsgctn = document.getElementsByClassName("_3zb-j").length - 1
+  var lastMSG document.getElementsByClassName("_3zb-j")[lastmsgctn].textContent
+  return lastMSG
 }
